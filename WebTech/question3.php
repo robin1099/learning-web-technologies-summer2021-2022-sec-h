e07f064ca4d94d18b83c388ebe3034ad3b8ee1ea
robin1099 <106136964+robin1099@users.noreply.github.com>
2022-06-07 13:56:36 +0600

    Add files via upload

---
+++ WebTech/question3.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+	$number = 73;
+
+		if($number % 2 == 0){
+			echo "Even"; 
+		}
+		else{
+			echo "Odd";
+		}
+
+	
+
+
+?> 
+
+</body>
+</html>
---
+++ WebTech/question5.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+ 
+	$num = 11;
+		while ( $num < 100 ) {
+  		echo "$num",",";
+  		$num += 2;
+		}
+ 
+?> 
+
+</body>
+</html>
